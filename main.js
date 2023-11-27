b04484bdff1adc675ebea2727cf387c7f5db8ddb
Admin <reanajb@gmail.com>
2023-11-27 12:41:42 -0500

    finished homework

--- main.js
+++ main.js
@@ -1,7 +1,64 @@
 /********************
  * YOUR CODE BELOW! *
  ********************/
+function moreThan5(num){
+return num > 5
+}
 
+// function isNewTopScore(score, topscore){
+//   return score >=15 && topscore <=18
+// } this passed but correct syntax below
+function isNewTopScore(score, topscore){
+  return score > topscore
+}
+
+function isInDanger(gradeD){
+  return gradeD >=60 && gradeD <=71
+}
+
+function isCoasting(gradeC){
+  return gradeC >=72 && gradeC <=83
+}
+
+function isSucceeding(gradeB){
+  return gradeB >=84 && gradeB <=92
+}
+
+function isFailing(gradeF){
+  return gradeF < 60  
+}
+
+function isAcing(gradeA){
+  return gradeA > 92
+}
+
+function isStudent(role1){
+  return role1 === "student"
+}
+
+function isTeacher(role2){
+  return role2 === "teacher"
+}
+
+function isAdmin(role3){
+  return role3 === "admin"
+}
+
+function isElementary(schoolLevel){
+  return schoolLevel === "elementary"
+}
+
+function areDifferentPeople(name1, name2){
+  return name1 !== name2
+}
+
+function isMiddleSchoolTeacher(role, schoolLevel){
+  return (role === "teacher") && (schoolLevel >= 6 && schoolLevel <= 8)
+}
+
+function notAnElementarySchoolAdministrator(schoolLevel, role){
+  return (schoolLevel !== "elementary") || (role !== "admin") 
+}
 
 
 /********************************************************************************************
